94165e0832174d4acffc74ddb0933e05b9dee7e6
Enrico Fasoli <enrico.fasoli1996@gmail.com>
2018-02-02 14:50:27 +0100

    removed react-create-app example code

--- src/App.js
+++ src/App.js
@@ -1,6 +1,4 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
 import WithChluServiceNode from './components/WithChluServiceNode'
 import EventLog from './components/EventLog'
 
@@ -9,14 +7,7 @@ const ChluEventLog = WithChluServiceNode(EventLog)
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
         <ChluEventLog />
       </div>
     );
